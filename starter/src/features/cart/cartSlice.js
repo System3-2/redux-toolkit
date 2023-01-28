0095eb57e7435b023e5a82c9f2524aa5e241e272
OlojaMuyiwa <olojam4969@gmail.com>
2023-01-28 21:13:20 +0100

    modal slice

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -14,12 +14,42 @@ const cartSlice = createSlice({
   reducers: {
     clearCart: (state) => {
       state.cartItems = []
+    },
+    removeItem: (state, action) => {
+      console.log(action);
+      const itemId = action.payload;
+
+      state.cartItems = cartItems.filter(item => {
+        return item.id !== itemId;
+      });
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find(item => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find(item => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+      if (cartItem.amount === 0) {
+        return cartItem.amount = 0;
+      }
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+
+      state.cartItems.forEach(item => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
     }
   }
 });
 
 // console.log(cartSlice);
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, decrease, increase, calculateTotals } = cartSlice.actions;
 
 export default cartSlice.reducer;
